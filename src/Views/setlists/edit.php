f6df51f6abe69ff516e8f7a9c25d784fa19e1049
Finn <finnkit05@gmail.com>
2026-02-09 11:15:26 +0100

    Branch Reordering

--- src/Views/setlists/edit.php
+++ src/Views/setlists/edit.php
@@ -3,8 +3,8 @@ $backURI = "/setlists/".$setlist['id'];
 require __DIR__ . "/../layout/topBarEdit.php";
 ?>
 
-<link rel="stylesheet" href="/css/setlists.css">
 <link rel="stylesheet" href="/css/edit.css">
+<link rel="stylesheet" href="/css/setlists.css">
 
 <section class="section edit">
     <form id="editForm" action="/setlists/<?= $setlist['id'] ?>/edit" method="post">
@@ -117,6 +117,10 @@ require __DIR__ . "/../layout/topBarEdit.php";
         </div>
         <svg class="delete" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-trash2-icon lucide-trash-2"><path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/></svg>
     </div>
+</section>
+
+<section>
+    
 </section>
 
 <script>
